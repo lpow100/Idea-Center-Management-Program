eb1c9b0f3a7600f37d5f7d1a1f054ec7811918ac
lpow100 <lgn.kocher@gmail.com>
2026-05-11 10:21:08 -0700

    made run.bat run both, not tested however

--- run.bat
+++ run.bat
@@ -1,3 +1,7 @@
-cd frontend
-run.bat
-cd ..
+@echo off
+powershell -Command ^
+  "$j1 = Start-Job { Set-Location '%~dp0frontend'; .\run.bat }; ^
+   $j2 = Start-Job { Set-Location '%~dp0backend'; .\main.exe }; ^
+   try { Wait-Job $j1, $j2 } ^
+   finally { Stop-Job $j1, $j2; Remove-Job $j1, $j2; Write-Host 'Stopped everything.' }"
+pause
